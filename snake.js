fb94078d664c01c78720be3419b531e1dce91b8a
SigestIoT <117729079+SigestIoT@users.noreply.github.com>
2024-07-16 15:45:14 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -40,7 +40,7 @@ function Snake() {
     this.tail = [];
 
     this.draw = function() {
-        ctx.fillStyle = "#FFFFFF";
+        ctx.fillStyle = "#000000";
         for (let i = 0; i < this.tail.length; i++) {
             ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
         }
