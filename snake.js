a5e62db19489dbb2e383c8629018f2418802cd67
SigestIoT <117729079+SigestIoT@users.noreply.github.com>
2024-07-16 15:44:11 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -5,7 +5,7 @@ const scale = 20;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
-let snake;
+let snake, fruit;
 
 (function setup() {
     snake = new Snake();
@@ -41,7 +41,7 @@ function Snake() {
 
     this.draw = function() {
         ctx.fillStyle = "#FFFFFF";
-        for (let i=0; i<this.tail.length; i++) {
+        for (let i = 0; i < this.tail.length; i++) {
             ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
         }
 
@@ -49,49 +49,55 @@ function Snake() {
     }
 
     this.update = function() {
-        for (let i=0; i<this.tail.length - 1; i++) {
-            this.tail[i] = this.tail[i+1];
+        for (let i = 0; i < this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i + 1];
         }
 
-        this.tail[this.total - 1] = { x: this.x, y: this.y };
+        if (this.total > 0) {
+            this.tail[this.total - 1] = { x: this.x, y: this.y };
+        }
 
         this.x += this.xSpeed;
         this.y += this.ySpeed;
 
-        if (this.x > canvas.width) {
+        if (this.x >= canvas.width) {
             this.x = 0;
+        } else if (this.x < 0) {
+            this.x = canvas.width - scale;
         }
 
-        if (this.y > canvas.height) {
+        if (this.y >= canvas.height) {
             this.y = 0;
-        }
-
-        if (this.x < 0) {
-            this.x = canvas.width;
-        }
-
-        if (this.y < 0) {
-            this.y = canvas.height;
+        } else if (this.y < 0) {
+            this.y = canvas.height - scale;
         }
     }
 
     this.changeDirection = function(direction) {
-        switch(direction) {
+        switch (direction) {
             case 'Up':
-                this.xSpeed = 0;
-                this.ySpeed = -scale * 1;
+                if (this.ySpeed === 0) {
+                    this.xSpeed = 0;
+                    this.ySpeed = -scale * 1;
+                }
                 break;
             case 'Down':
-                this.xSpeed = 0;
-                this.ySpeed = scale * 1;
+                if (this.ySpeed === 0) {
+                    this.xSpeed = 0;
+                    this.ySpeed = scale * 1;
+                }
                 break;
             case 'Left':
-                this.xSpeed = -scale * 1;
-                this.ySpeed = 0;
+                if (this.xSpeed === 0) {
+                    this.xSpeed = -scale * 1;
+                    this.ySpeed = 0;
+                }
                 break;
             case 'Right':
-                this.xSpeed = scale * 1;
-                this.ySpeed = 0;
+                if (this.xSpeed === 0) {
+                    this.xSpeed = scale * 1;
+                    this.ySpeed = 0;
+                }
                 break;
         }
     }
@@ -105,7 +111,7 @@ function Snake() {
     }
 
     this.checkCollision = function() {
-        for (let i=0; i<this.tail.length; i++) {
+        for (let i = 0; i < this.tail.length; i++) {
             if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
                 this.total = 0;
                 this.tail = [];
@@ -119,12 +125,12 @@ function Fruit() {
     this.y;
 
     this.pickLocation = function() {
-        this.x = (Math.floor(Math.random() * columns - 1) + 1) * scale;
-        this.y = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+        this.x = Math.floor(Math.random() * columns) * scale;
+        this.y = Math.floor(Math.random() * rows) * scale;
     }
 
     this.draw = function() {
         ctx.fillStyle = "#FF0000";
-        ctx.fillRect(this.x, this.y, scale, scale)
+        ctx.fillRect(this.x, this.y, scale, scale);
     }
 }
